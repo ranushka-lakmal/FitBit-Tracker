3b4fb85f28e1459ab10206bd2792e6b2b896aff8
HCS-LK-LT-0225\ranushka.l <ranushka.lakmal@hitachi-dps.com>
2023-07-17 00:49:08 +0530

    cheat meal ui

--- FitBit Tracker/CheatMealForm.Designer.cs
+++ FitBit Tracker/CheatMealForm.Designer.cs
@@ -183,9 +183,10 @@
             label1.AutoSize = true;
             label1.Location = new Point(37, 79);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(18, 15);
             label1.TabIndex = 0;
-            label1.Text = "label1";
+            label1.Text = "ID";
+            label1.Click += label1_Click;
             // 
             // listView1
             // 
--- FitBit Tracker/CheatMealForm.cs
+++ FitBit Tracker/CheatMealForm.cs
@@ -26,5 +26,10 @@ namespace FitBit_Tracker
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
